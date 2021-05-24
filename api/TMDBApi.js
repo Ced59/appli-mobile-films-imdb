c8537e98556bed0b580fe856c26e530e7c1233da
Cedric <c.caudron59@gmail.com>
2021-05-24 11:59:29 +0200

    get film detail from API OK

--- api/TMDBApi.js
+++ api/TMDBApi.js
@@ -1,12 +1,21 @@
 const API_TOKEN = "d9f3e64a71289b0ce3eb8e272b1836d6";
 
 export function getFilmsFromApiWithText(text, page) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text+'&page='+page;
-  return fetch( url )
-    .then((response) => response.json())
-    .catch((error) => console.log(error))
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page=' + page;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
+}
+
+export async function getFilmFromApiById(idFilm) {
+    const url = 'https://api.themoviedb.org/3/movie/' + idFilm + '?api_key=' + API_TOKEN + '&language=fr';
+    console.log(url);
+
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.log(error));
 }
 
 export function getImageFromApi(name) {
-  return "https://image.tmdb.org/t/p/w500"+name;
+    return "https://image.tmdb.org/t/p/w500" + name;
 }
--- components/Search.js
+++ components/Search.js
@@ -1,116 +1,118 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Button, FlatList, Text, ActivityIndicator } from 'react-native';
-
-import films from '../Helpers/filmsData'
+import {ActivityIndicator, Button, FlatList, StyleSheet, TextInput, View} from 'react-native';
 import FilmItem from './FilmItem'
 import {getFilmsFromApiWithText} from '../api/TMDBApi'
 
 
 const styles = StyleSheet.create({
-  textinput: {
-    borderColor: 'black' ,
-    borderWidth:1,
-    marginLeft: 5,
-    marginRight:5,
-    height:50
-  },
-  margin_top: {
-    marginTop: 20
-  },
-  loading_container: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 100,
-    bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    textinput: {
+        borderColor: 'black',
+        borderWidth: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        height: 50
+    },
+    margin: {
+        marginTop: 5,
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 15
+    },
+    listFilms: {
+        marginBottom: 120
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 200,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 
 class Search extends React.Component {
 
-  constructor(props) {
-    super(props);
-    //  this._films = [];
-    // utilisation de state
-    this.searchedText =  "";
-    this.page = 0; // page courante
-    this.totalPages = 0; // nombre de page totale
-    this.state = { films : [], isLoading: false }
-  }
-
-_displayDetailForFilm = (idFilm) => {
-  console.log("Display detail "+ idFilm);
-  this.props.navigation.navigate('FilmDetail');
-}
-
-_searchFilms(){
-  this.page = 0;
-  this.totalPages=0;
-  this.setState({films: []});
-  this._loadFilms();
-}
-
-  _loadFilms(){
-    this.setState({isLoading:true});
-
-  setTimeout(() =>   getFilmsFromApiWithText(this.searchedText, this.page+1).then(data =>
-    {
-      this.page = data.page;
-      this.totalPages = data.total_pages;
-      this.setState({
-      //  films: this.state.films.concat(data.results),
-        films: [...this.state.films, ...data.results],
-        isLoading: false
-        })
+    constructor(props) {
+        super(props);
+        this.searchedText = "";
+        this.page = 0;
+        this.totalPages = 0;
+        this.state = {films: [], isLoading: false}
     }
-  ) , 2000);
-  }
 
-  _searchedTextInputChanged(text) {
-    this.searchedText = text;
-  }
-
-  _displayLoading() {
-    if (this.state.isLoading) {
-
-      return (
-        <View style={styles.loading_container}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
-      )
+    _displayDetailForFilm = (idFilm) => {
+        console.log("Display detail " + idFilm);
+        this.props.navigation.navigate('FilmDetail', {idFilm});
     }
-  }
 
-  render() {
-    console.log(this.state.isLoading)
-    return (
-      <View style={styles.margin_top}>
-        <TextInput style={[styles.textinput, styles.margin_top]}
-        placeholder='Titre du film'
-        onChangeText={(text) => this._searchedTextInputChanged(text)}
-        onSubmitEditing={() => this._searchFilms()} />
-        <Button title='Rechercher' onPress={() => this._searchFilms()} />
+    _searchFilms() {
+        this.page = 0;
+        this.totalPages = 0;
+        this.setState({films: []});
+        this._loadFilms();
+    }
 
-        <FlatList
-          data={this.state.films}
-          keyExtractor={(item) =>  item.id.toString()  }
-          renderItem={( {item} ) => <FilmItem displayDetailForFilm={this._displayDetailForFilm} film={item} /> }
-          onEndReachedThreshold={1}
-          onEndReached={() => {
-            //console.log("Limite atteinte")
-            if (this.page < this.totalPages){
-              this._loadFilms()
+    _loadFilms() {
+        this.setState({isLoading: true});
+
+        setTimeout(() => getFilmsFromApiWithText(this.searchedText, this.page + 1).then(data => {
+                this.page = data.page;
+                this.totalPages = data.total_pages;
+                this.setState({
+                    films: [...this.state.films, ...data.results],
+                    isLoading: false
+                })
             }
-          }}
-          />
+        ), 2000);
+    }
 
-          {this._displayLoading()}
-      </View>
-    );
-  }
+    _searchedTextInputChanged(text) {
+        this.searchedText = text;
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large" color="#0000ff"/>
+                </View>
+            )
+        }
+    }
+
+    render() {
+
+        return (
+            <View style={styles.margin}>
+                <TextInput style={[styles.textinput, styles.margin]}
+                           placeholder='Titre du film'
+                           onChangeText={(text) => this._searchedTextInputChanged(text)}
+                           onSubmitEditing={() => this._searchFilms()}/>
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+
+                <FlatList
+                    style={styles.listFilms}
+                    data={this.state.films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem displayDetailForFilm={this._displayDetailForFilm} film={item}/>}
+                    onEndReachedThreshold={1}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }}
+                />
+
+                {this._displayLoading()}
+            </View>
+        );
+    }
 
 }
 
